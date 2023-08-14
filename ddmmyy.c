5ee52d2a7a31018663acbc1e249c9f60ba8e1132
Avneesh Kumar <kaviikumar010@gmail.com>
2023-08-14 23:09:26 +0530

    new one

---
+++ ddmmyy.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int arr[]={31,28,31,30,31,30,31,31,30,31,30,31};
+    printf("Enter the date \n");
+    char date[11];
+    char * token;
+    scanf("%s", date); // here we are not use address sing because date is already a address
+    token =strtok(date,"-");
+    // strtok is use  for break a string...... strtok string ko break krta hai
+    token=strtok(NULL,"-");
+    int i= atoi(token);
+    printf("Month has %d days",arr[i-1]);
+    return 0;
+}
