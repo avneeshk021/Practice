377ce1fe94366bd709681e2904b2cf7cd30356ca
Avneesh Kumar <kaviikumar010@gmail.com>
2023-08-27 02:29:14 +0530

    new

--- Pattern/Rectpattern.cpp
+++ Pattern/Rectpattern.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main()
 {
-    for(int row=0; row<3; row++)
+    for(int row=0; row<3; row++)// Outter Loop (for row)
     {
-        for(int col=0; col<5; col++)
+        for(int col=0; col<5; col++)// inner loop ( for columb)
         {
             cout<<"*";
         }
