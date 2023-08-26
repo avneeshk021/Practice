ac6da58efef4ab7f9d53bec5faff0b3a7925e03e
Avneesh Kumar <kaviikumar010@gmail.com>
2023-08-27 02:31:49 +0530

    another

---
+++ Pattern/sqrPatt.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int row=0; row<5; row++)// Outter Loop (for row)
+    {
+        for(int col=0; col<5; col++)// inner loop ( for columb)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
