46ccf088e68b4831b857742e3dd738b5b4452bf7
Avneesh Kumar <kaviikumar010@gmail.com>
2023-08-27 12:02:08 +0530

    new

--- Pattern/Halfprymid.cpp
+++ Pattern/Halfprymid.cpp
@@ -4,12 +4,21 @@ using namespace std;
 int main()
 {
     
+    // for(int row=0; row<7; row++)
+    // {
+    //  for(int col=0; col<row+1; col++)
+    //  {
+    //     cout<<"* ";
+    //  }   
+    //  cout<<endl;
+    // }
     for(int row=0; row<7; row++)
     {
-     for(int col=0; col<row+1; col++)
+     for(int col=0; col<7-row; col++)
      {
         cout<<"* ";
      }   
      cout<<endl;
     }
+
 }
