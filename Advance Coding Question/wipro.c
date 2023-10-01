a176d9036e0cb42523794b3dd11126feb0fde2f5
Avneesh Kumar <kaviikumar010@gmail.com>
2023-10-01 14:28:08 +0530

    update

--- Advance Coding Question/wipro.c
+++ Advance Coding Question/wipro.c
@@ -1,4 +1,4 @@
-/*etha like chocolates an he loves to try different onces there are N shops in a  market lablled from 1 t N and each shops offers a different variety of chocolate jetha start from ith shop  Jetha would like to know how many more shops he has to visit you have been give L dentnoting number of bits required to represent N you need to return the maximum number of shops he can visit
+/*jetha like chocolates an he loves to try different onces there are N shops in a  market lablled from 1 t N and each shops offers a different variety of chocolate jetha start from ith shop  Jetha would like to know how many more shops he has to visit you have been give L dentnoting number of bits required to represent N you need to return the maximum number of shops he can visit
 Example
 Input i=2 L=3
 output 6
