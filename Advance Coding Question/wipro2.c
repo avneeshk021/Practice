0c96cc3a2d4205ecddeab4569833e06bd4159303
Avneesh Kumar <kaviikumar010@gmail.com>
2023-10-02 16:13:55 +0530

    new

--- Advance Coding Question/wipro2.c
+++ Advance Coding Question/wipro2.c
@@ -1,4 +1,5 @@
 /* Given a positive interger N point count of set bit in it 
+Wipro NLTH
 Example 1
 input N=6
 output
@@ -9,24 +10,44 @@ output
 1
 
 */
-#include <stdio.h>
+// #include <stdio.h>
  
 
-int countSetBits(int n)
+// int countSetBits(int n)
+// {
+//     // base case
+//     if (n == 0)
+//         return 0;
+//     else
+//         // if last bit set add 1 else add 0
+//         return (n & 1) + countSetBits(n >> 1);
+// }
+ 
+
+// int main()
+// {
+//     int n = 9;
+//     // function calling
+//     printf("%d", countSetBits(n));
+//     return 0;
+// }
+#include<stdio.h>
+unsigned int setbit(unsigned int n)
 {
-    // base case
-    if (n == 0)
-        return 0;
-    else
-        // if last bit set add 1 else add 0
-        return (n & 1) + countSetBits(n >> 1);
+    unsigned int count=0;
+    while (n)
+    {
+        count=count+(n&1);
+        n/2; //right shift n=n>>1;
+    }
+    return count;
+    
 }
- 
-
 int main()
 {
-    int n = 9;
-    // function calling
-    printf("%d", countSetBits(n));
+    int i;
+    scanf("%d",&i);
+    printf("%d", setbit(i));
     return 0;
 }
+
